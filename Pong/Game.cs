6044d9ba54455d9992e5941c6c7ed4ace98f3152
Stoyan-1926 <stoyan.kaev.pp26@pgmtplovdiv.info>
2025-06-04 08:47:46 +0300

    moving  ball

--- Pong/Game.cs
+++ Pong/Game.cs
@@ -27,7 +27,11 @@ namespace Pong
         {
             ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
                                        _graphics.PreferredBackBufferHeight / 2);
+           
+
             ballSpeed = 100f;
+            ballSpeedVector = new Vector2(-1 , -1);
+
 
             //TODO: инициализирайте вектора на скоростта ballSpeedVector, за да зададете началната посока на движение
 
@@ -48,14 +52,27 @@ namespace Pong
             float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             //TODO: Изменете ballPosition.X и ballPosition.Y в зависимост от посоката на движение
+            //float updatedBall = ballPosition.X * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float updatedBallPosition = ballSpeedVector.Y * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            // ballPosition += ballSpeedVector * updatedBallSpeed;
+
+            ballPosition.X +=  updatedBallSpeed * ballSpeedVector.X;
+            ballPosition.Y +=  updatedBallSpeed * ballSpeedVector.Y;
 
             //TODO: Натрупайте остатъците получени от закръглянето в променливите remainderX и remainderY
 
-
             //TODO: Ако картинката напуска границите на екрана, това означава, че топката се е "ударила" в края на екрана и трябва да
             //промени посоката си на движение. За целта трябва да промените вектора ballSpeedVector.
-
-
+            int screenWidth = _graphics.PreferredBackBufferWidth;
+            int screenHeight = _graphics.PreferredBackBufferWidth;
+            if (ballPosition.X <= 0 || ballPosition.X + ballTexture.Width >=_graphics.PreferredBackBufferWidth )
+            {
+                ballSpeedVector.X *= -1;
+            }
+            if ( ballPosition.Y <= 0 || ballPosition.Y + ballTexture.Height >=_graphics.PreferredBackBufferHeight )
+            {
+                ballSpeedVector.Y *= -1;
+            }
             base.Update(gameTime);
         }
 
